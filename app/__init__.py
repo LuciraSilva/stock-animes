45218fdf974f1ee11444d20f57d9b06b84b5fd26
Lucira Silva <luciracs0@gmail.com>
2021-09-14 18:13:43 -0300

    feat: add blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
+from app.services import create_animes_table, create_db
+from app import views
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
+    create_db()
+    
+    create_animes_table()
+
+    views.init_app(app)
+    
     return app
